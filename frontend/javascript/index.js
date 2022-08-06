ac7fcb268ab59496ec9d05df5cbf87fd6be3c63e
Jamie Macey <jamie.git@tracefunc.com>
2022-08-06 12:55:10 -0700

    Upgrade bridgetown gems, rebuild skeleton

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,8 +1,7 @@
+import "index.css"
+import "syntax-highlighting.css"
 
-import "index.scss"
-
-// // Import all javascript files from src/_components
-// const componentsContext = require.context("bridgetownComponents", true, /.js$/)
-// componentsContext.keys().forEach(componentsContext)
+// Import all JavaScript & CSS files from src/_components
+import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
 console.info("Bridgetown is loaded!")
