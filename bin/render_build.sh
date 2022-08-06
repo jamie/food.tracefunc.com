30812060dd4090099ddd608fd0620b57d07d1d2f
Jamie Macey <jamie.git@tracefunc.com>
2022-08-06 14:54:26 -0700

    Update buildscript, put cook binary into PATH

--- bin/render_build.sh
+++ bin/render_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo PATH
-echo $PATH
-
 # Install cooklang
+mkdir local-bin
 curl -LO https://github.com/cooklang/CookCLI/releases/download/v0.1.6/CookCLI_0.1.6_linux_amd64.zip
 unzip CookCLI*.zip
+mv cook local-bin
+export PATH=$PWD/local-bin:$PATH
 
 bundle exec bridgetown deploy
